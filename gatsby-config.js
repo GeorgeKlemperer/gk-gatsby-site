fcf6e18b6ea36ca8c37bddfe75827cabfc712d91
GeorgeKlemperer <george@klemperer.co.uk>
2023-10-30 11:15:21 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `GK Gatsby Site`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
